f4cc21e539f1413efc09c2a6c4d19ed9f3f31dfc
Emil Cardell <emil.cardell@gmail.com>
2010-06-13 14:28:03 +0200

    Added missing IOPermissionFunctions.cs and MsSqlFunctions.cs

---
+++ src/Phantom.Core/Builtins/IOPermissionFunctions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.AccessControl;
+using System.Runtime.CompilerServices;
+
+namespace Phantom.Core.Builtins
+{
+    [CompilerGlobalScope]
+    public static class IOPermissionFunctions
+    {
+        public static void SetFilePermission(string path, string accountName, PermissionLevel permissionLevel)
+        {
+            AddFileSecurity(path, accountName, GetRightsFromPermissionLevel(permissionLevel));
+        }
+
+        public static void SetDirectoryPermission(string path, string accountName, PermissionLevel permissionLevel)
+        {
+            AddDirectorySecurity(path, accountName, GetRightsFromPermissionLevel(permissionLevel));
+        }
+
+        private static void AddFileSecurity(string path, string account, FileSystemRights rights)
+        {
+            FileInfo fileInfo = new FileInfo(path.Replace('\\', '/'));
+            FileSecurity fileSecurity = fileInfo.GetAccessControl();
+            fileSecurity.AddAccessRule(new FileSystemAccessRule(account,
+                                                            rights,
+                                                            AccessControlType.Allow
+                                                            ));
+
+            fileInfo.SetAccessControl(fileSecurity);
+        }
+
+        private static void AddDirectorySecurity(string path, string account, FileSystemRights rights)
+        {
+            DirectoryInfo direcotryInfo = new DirectoryInfo(path.Replace('\\', '/'));
+            DirectorySecurity directorySecurity = direcotryInfo.GetAccessControl();
+            directorySecurity.AddAccessRule(new FileSystemAccessRule(account,
+                                                            rights, InheritanceFlags.ContainerInherit, PropagationFlags.InheritOnly,
+                                                            AccessControlType.Allow));
+            directorySecurity.AddAccessRule(new FileSystemAccessRule(account,
+                                                            rights, InheritanceFlags.ObjectInherit, PropagationFlags.InheritOnly,
+                                                            AccessControlType.Allow));
+            directorySecurity.AddAccessRule(new FileSystemAccessRule(account,
+                                                            rights, AccessControlType.Allow));
+            direcotryInfo.SetAccessControl(directorySecurity);
+
+        }
+
+        private static FileSystemRights GetRightsFromPermissionLevel(PermissionLevel level)
+        {
+            if (level == PermissionLevel.Full)
+                return FileSystemRights.FullControl;
+
+            if (level == PermissionLevel.Modify)
+                return FileSystemRights.Modify;
+
+            if (level == PermissionLevel.Write)
+                return FileSystemRights.Write;
+
+            if (level == PermissionLevel.ReadAndExecute)
+                return FileSystemRights.ReadAndExecute;
+
+            return FileSystemRights.Read;
+        }
+
+    }
+
+    public enum PermissionLevel
+    {
+        Read,
+        ReadAndExecute,
+        Write,
+        Modify,
+        Full
+    }
+}
---
+++ src/Phantom.Core/Builtins/MsSqlFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Data.SqlClient;
+
+namespace Phantom.Core.Builtins
+{
+    [CompilerGlobalScope]
+    public sealed class MsSqlFunctions
+    {
+        public static void MsSql_Create_Database(string databaseName, string connectionString)
+        {
+            if (MsSql_Database_Exists(databaseName, connectionString))
+                return;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("create database " + databaseName, connection);
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public static bool MsSql_Database_Exists(string databaseName, string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("select count(1) from sys.databases where name = '" + databaseName + "'", connection);
+                connection.Open();
+                int count = (int)sqlCommand.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        public static void MsSql_Delete_Database(string databaseName, string connectionString)
+        {
+            if (!MsSql_Database_Exists(databaseName, connectionString))
+                return;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("drop database " + databaseName, connection);
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
